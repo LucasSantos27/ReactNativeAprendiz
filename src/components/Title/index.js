84e56c4d6b4f27b9e45337590ca362e896bf9ce8
Lucas-megaleios <86004242+Lucas-megaleios@users.noreply.github.com>
2021-07-30 11:09:01 -0300

    Created first component

---
+++ src/components/Title/index.js
@@ -0,0 +1,11 @@
+import { func } from "prop-types"
+import React from "react"
+import {View, Text} from "react-native"
+
+export default function Title(){
+  return(
+    <View>
+      <Text>Calcule seu IMC</Text>
+    </View>
+  );
+}
